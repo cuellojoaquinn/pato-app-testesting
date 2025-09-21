dc2c8167608238948376277bc3691a74bce54402
Hernan Dario Castro <3psdariocastro@gmail.com>
2025-09-21 16:18:15 -0300

    Update deploy-prod.sh

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -7,6 +7,6 @@ echo ":rocket: Iniciando deploy a Producción (Vercel)..."
 # se toma automáticamente. Si no, se puede pasar con --scope
 # o variables de entorno.
 
-npx vercel --prod --token= 4rPLHoxJNbt0f8gjII0Zkanc --confirm
+npx vercel --prod --token= $VERCEL_TOKEN --confirm
 
 echo ":white_check_mark: Deploy a Producción completado."
